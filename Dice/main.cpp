279c72eab4dcef6adf040ea715f01f9d13988cb2
藤井 陽介 <fujiiyousuke@SyousaMacBookAir.local>
2013-12-13 21:29:59 +0900

    Initial Commit

---
+++ Dice/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Dice
+//
+//  Created by 藤井 陽介 on 2013/12/13.
+//  Copyright (c) 2013年 syo-sa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
